0fa76dbb14cd1ae91ea2f17904ec013586df17c9
Sam Dayo <samueldayo2017@gmail.com>
2023-11-10 14:43:40 -0500

    Initial Commit. Functional Joystick Implementation

--- main.c
+++ main.c
@@ -21,7 +21,14 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <time.h>
+#include "stdio.h"
+#include "string.h"
+//#include "stm32l475e_iot01_gyro.h"
+//#include "stm32l475e_iot01_accelero.h"
+//#include "stm32l475e_iot01.h"
+//#include "mx25r6435f.h"
+//#include "stm32l475e_iot01_qspi.h"
+//#include "JOYSTICK.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -31,8 +38,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define N_COLS 5
-#define N_ROWS 10
+#define JOYSTICK1 0
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -41,99 +47,50 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-RNG_HandleTypeDef hrng;
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+
+I2C_HandleTypeDef hi2c2;
+
+QSPI_HandleTypeDef hqspi;
+
+TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
+//float gyro[3];
+char str_hum[100] = "";
+//int16_t accel[3];
+
+volatile uint16_t joystickXY[2]; //12 bit ADC so we use 16 bit unsigned ints
+const int num_inputs = 2;
+volatile int conversion_cmplt = 0; //set by the call back
+enum state{
+	straight, left, right
+};
+
+int centered = 0;
+
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_I2C2_Init(void);
 static void MX_USART1_UART_Init(void);
-static void MX_RNG_Init(void);
+static void MX_QUADSPI_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-char clear_screen[5] = {0x1B, 0x5B, 0x32, 0x4A, 0x00};
 
-// TODO: Make variables names more descriptive
-uint8_t char_horz_pos = 5;
-uint8_t char_vert_pos = 1;
-char map[N_ROWS][2 * N_COLS + 1 + 3] = {
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-    "| | | | | |\r\n",
-};
-
-uint8_t top_row = 0;
-
-uint8_t expected_lane = 5; // Character starts in lane 5
-
-void update_map() {
-  uint32_t rand_num;
-  HAL_RNG_GenerateRandomNumber(&hrng, &rand_num);
-
-  uint8_t past_expected_lane = expected_lane;
-  if (rand_num & 1) { // Determine if expected lane changes
-    if (expected_lane == 1) {
-      expected_lane = 3;
-    }
-    else if (expected_lane == 9) {
-      expected_lane = 7;
-    }
-    else { // Determine if going right or left
-      if ((rand_num >> 2) & 1) {
-        expected_lane += 2;
-      }
-      else {
-        expected_lane -= 2;
-      }
-    }
-  }
-
-  for (int col = 1; col <= 2 * N_COLS - 1; col += 2) {
-    if (col == past_expected_lane || col == expected_lane) {
-      map[top_row][col] = ' ';
-      continue;
-    }
-
-    if ((rand_num >> col) & 1) {
-      map[top_row][col] = 'X';
-    }
-    else {
-      map[top_row][col] = ' ';
-    }
-  }
-  top_row = (top_row + 1) % N_ROWS;
-}
-
-void display_map(uint8_t start_row) {
-  // First, clear console
-  HAL_UART_Transmit(&huart1, (uint8_t*) clear_screen, sizeof(clear_screen), 1000);
-
-//  char real_char = map[(start_row + char_vert_pos) % 9][char_horz_pos];
-//  map[(start_row + char_vert_pos) % 9][char_horz_pos] = 'O';
-
-  // Display map
-  for (int row = 0; row < N_ROWS; row++) {
-    HAL_UART_Transmit(&huart1, (uint8_t*) map[(start_row + row) % N_ROWS], sizeof(map[row]), 1000);
-  }
-
-//  map[(start_row + char_vert_pos) % 9][char_horz_pos] = real_char;
-}
 /* USER CODE END 0 */
 
 /**
@@ -143,7 +100,6 @@ void display_map(uint8_t start_row) {
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -164,9 +120,17 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_I2C2_Init();
   MX_USART1_UART_Init();
-  MX_RNG_Init();
+  MX_QUADSPI_Init();
+  MX_ADC1_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
+
+  HAL_ADC_Start_DMA(&hadc1, (uint32_t* ) joystickXY, 2);
+  HAL_TIM_Base_Start_IT(&htim3);
+  enum state mode = straight;
 
   /* USER CODE END 2 */
 
@@ -174,18 +138,49 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    display_map(top_row);
-    HAL_Delay(750);
-    update_map();
-//    if (char_horz_pos == 9) {
-//      char_horz_pos = 1;
-//    }
-//    else {
-//      char_horz_pos += 2;
-//    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  if (joystickXY[0] < 3000 && joystickXY[1] > 1000){
+		  mode = straight;
+		  centered = 1;
+	  }
+	  if(joystickXY[0] >= 3000 && centered == 1){
+		  mode = left;
+		  centered = 0;
+	  }
+	  if(joystickXY[0] <= 1000 && centered == 1){
+		  mode = right;
+		  centered = 0;
+	  }
+
+	  switch(mode){
+	  	  case straight:
+	  		memset(str_hum, '\0', sizeof(str_hum));
+	  		snprintf(str_hum, 100, "You're going straight...be careful of obstacles!\n\r");
+	  		HAL_UART_Transmit(&huart1, (uint8_t*) str_hum, sizeof(str_hum), HAL_MAX_DELAY);
+	  		HAL_Delay(500);
+	  		break;
+	  	  case left:
+	  		memset(str_hum, '\0', sizeof(str_hum));
+	  		snprintf(str_hum, 100, "Okay, moving to the left!\n\r");
+	  		HAL_UART_Transmit(&huart1, (uint8_t*) str_hum, sizeof(str_hum), HAL_MAX_DELAY);
+	  		HAL_Delay(500);
+	  		break;
+	  	  case right:
+	  		memset(str_hum, '\0', sizeof(str_hum));
+	  		snprintf(str_hum, 100, "Okay, moving to the right!\n\r");
+	  		HAL_UART_Transmit(&huart1, (uint8_t*) str_hum, sizeof(str_hum), HAL_MAX_DELAY);
+	  		HAL_Delay(500);
+	  		break;
+	  	  default:
+	  }
+//	  memset(str_hum, '\0', sizeof(str_hum));
+//	  snprintf(str_hum, 100, "The joystick reports that: x = %d, y = %d\n\r",joystickXY[0],joystickXY[1]);
+//	  HAL_UART_Transmit(&huart1, (uint8_t*) str_hum, sizeof(str_hum), HAL_MAX_DELAY);
+//	  HAL_Delay(500);
+
+
   }
   /* USER CODE END 3 */
 }
@@ -241,28 +236,217 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief RNG Initialization Function
+  * @brief ADC1 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_RNG_Init(void)
+static void MX_ADC1_Init(void)
 {
 
-  /* USER CODE BEGIN RNG_Init 0 */
+  /* USER CODE BEGIN ADC1_Init 0 */
 
-  /* USER CODE END RNG_Init 0 */
+  /* USER CODE END ADC1_Init 0 */
 
-  /* USER CODE BEGIN RNG_Init 1 */
+  ADC_MultiModeTypeDef multimode = {0};
+  ADC_ChannelConfTypeDef sConfig = {0};
 
-  /* USER CODE END RNG_Init 1 */
-  hrng.Instance = RNG;
-  if (HAL_RNG_Init(&hrng) != HAL_OK)
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.LowPowerAutoWait = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.NbrOfConversion = 2;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIG_T3_TRGO;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
+  hadc1.Init.DMAContinuousRequests = ENABLE;
+  hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc1.Init.OversamplingMode = DISABLE;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN RNG_Init 2 */
 
-  /* USER CODE END RNG_Init 2 */
+  /** Configure the ADC multi-mode
+  */
+  multimode.Mode = ADC_MODE_INDEPENDENT;
+  if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_13;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_24CYCLES_5;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_14;
+  sConfig.Rank = ADC_REGULAR_RANK_2;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x10909CEC;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief QUADSPI Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_QUADSPI_Init(void)
+{
+
+  /* USER CODE BEGIN QUADSPI_Init 0 */
+
+  /* USER CODE END QUADSPI_Init 0 */
+
+  /* USER CODE BEGIN QUADSPI_Init 1 */
+
+  /* USER CODE END QUADSPI_Init 1 */
+  /* QUADSPI parameter configuration*/
+  hqspi.Instance = QUADSPI;
+  hqspi.Init.ClockPrescaler = 255;
+  hqspi.Init.FifoThreshold = 1;
+  hqspi.Init.SampleShifting = QSPI_SAMPLE_SHIFTING_NONE;
+  hqspi.Init.FlashSize = 1;
+  hqspi.Init.ChipSelectHighTime = QSPI_CS_HIGH_TIME_1_CYCLE;
+  hqspi.Init.ClockMode = QSPI_CLOCK_MODE_0;
+  if (HAL_QSPI_Init(&hqspi) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN QUADSPI_Init 2 */
+
+  /* USER CODE END QUADSPI_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 10-1;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 8000-1;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
 
 }
 
@@ -302,17 +486,46 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(errorLED_GPIO_Port, errorLED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : errorLED_Pin */
+  GPIO_InitStruct.Pin = errorLED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(errorLED_GPIO_Port, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
@@ -329,6 +542,8 @@ static void MX_GPIO_Init(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
+	HAL_GPIO_WritePin(errorLED_GPIO_Port, errorLED_Pin, GPIO_PIN_RESET);
+	__BKPT();
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
